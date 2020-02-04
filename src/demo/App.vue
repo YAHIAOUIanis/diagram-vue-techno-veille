ab71eab5739b7bd7b6f86801bd8eac4975e7c829
Anis YAHIAOUI <yahiaoui.anis11@gmail.com>
2020-02-05 00:28:32 +0100

    initialiser le projet

---
+++ src/demo/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app"><DiagramEditor v-model="graph"></DiagramEditor></div>
+</template>
+
+<script>
+import data from "./data.json";
+import { DiagramEditor } from "../index.js";
+export default {
+  name: "app",
+  components: {
+    DiagramEditor
+  },
+  data() {
+    return {
+      graph: {}
+    };
+  },
+  mounted() {
+    this.graph = data;
+  }
+};
+</script>
+
+<style lang="scss">
+body {
+  #app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    // text-align: center;
+    color: #2c3e50;
+  }
+  .scrollXY {
+    overflow-x: auto;
+    overflow-y: auto;
+  }
+}
+</style>
